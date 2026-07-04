7b222173286296dc766492e998cd48f242a7367c
Sagor Chanda <ch.sagor27@gmail.com>
2026-07-05 01:11:33 +0600

    service class refactored

--- cli-task-tracker/src/services/TaskService.ts
+++ cli-task-tracker/src/services/TaskService.ts
@@ -2,42 +2,43 @@ import { Task } from "../models/Task";
 import { TaskRepository } from "../repositories/TaskRepository";
 
 export class TaskService {
-    private count: number;
-    constructor(private taskRepo: TaskRepository) {
-        this.count = 1;
-    }
-    addTask(description : string): Task {
+    constructor(private taskRepo: TaskRepository) {}
+    async addTask(description : string): Promise<Task> {
         if(!description.trim())
             throw new Error("Description can not be empty!");
+        const tasks = await this.taskRepo.getAll();
+        let nextId: number =0;
+        for(let task of tasks){
+            nextId = Math.max(Number(task.id), nextId);
+        }
         const now = new Date();
         const task: Task = {
-            id: String(this.count),
+            id: String(nextId+1),
             description: description,
             status: 'todo',
             createdAt: now,
             updatedAt: now
         }
-        this.count++;
         return this.taskRepo.save(task);
     }
-    getAllTask(): ReadonlyArray<Task> {
+    async getAllTask(): Promise<ReadonlyArray<Task>> {
         return this.taskRepo.getAll();
     }
-    getTaskById(id: string) {
+    async getTaskById(id: string) {
         return this.taskRepo.findById(id);
     }
-    updateTask(id: string, description: string): Task | null{
+    async updateTask(id: string, description: string): Promise<Task | null>{
         if(!description.trim())
             throw new Error("Description can not be empty!");
         return this.taskRepo.update(id, {description, updatedAt: new Date()});
     }
-    markInProgress(id: string): Task | null{
+    async markInProgress(id: string): Promise<Task | null>{
         return this.taskRepo.update(id, {status: 'in-progress', updatedAt: new Date()});
     }
-    markDone(id: string): Task | null{
+    async markDone(id: string): Promise<Task | null>{
         return this.taskRepo.update(id, {status: 'done', updatedAt: new Date()});
     }
-    deleteTask(id: string): boolean{
+    async deleteTask(id: string): Promise<boolean>{
         return this.taskRepo.delete(id);
     }
 }
