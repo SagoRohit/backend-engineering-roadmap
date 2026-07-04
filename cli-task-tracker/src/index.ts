285d72108d7f91d74cd2cc4b1d630c2224089c69
Sagor Chanda <ch.sagor27@gmail.com>
2026-07-05 03:31:38 +0600

    index page updated

--- cli-task-tracker/src/index.ts
+++ cli-task-tracker/src/index.ts
@@ -1,16 +1,25 @@
+import { CommandHandler } from "./commands/CommandHandler";
 import { TaskRepository } from "./repositories/TaskRepository";
 import { TaskService } from "./services/TaskService";
 
-console.log("Index start here...");
-const taskrepo = new TaskRepository;
-const taskService = new TaskService(taskrepo);
-taskService.addTask("buy-grocery");
-taskService.addTask("have meal");
-console.log("Before mark done.....");
-console.log(taskService.getAllTask());
-console.log('After mark done......');
-taskService.markDone("2");
-console.log(taskService.getAllTask());
-console.log("now update.....");
-taskService.updateTask('1', "buy-grocery and cook meal");
-console.log(taskService.getAllTask());
+// console.log("Index start here...");
+// const taskrepo = new TaskRepository;
+// const taskService = new TaskService(taskrepo);
+// taskService.addTask("buy-grocery");
+// taskService.addTask("have meal");
+// console.log("Before mark done.....");
+// console.log(taskService.getAllTask());
+// console.log('After mark done......');
+// taskService.markDone("2");
+// console.log(taskService.getAllTask());
+// console.log("now update.....");
+// taskService.updateTask('1', "buy-grocery and cook meal");
+// console.log(taskService.getAllTask());
+async function main() {
+    const cmd = new CommandHandler(
+        new TaskService(
+            new TaskRepository
+    ));
+    await cmd.run();
+}
+main();
