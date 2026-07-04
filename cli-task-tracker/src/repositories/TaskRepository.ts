528626385a7f8c6f11ed27c64be27d36ba937ad8
Sagor Chanda <ch.sagor27@gmail.com>
2026-07-05 00:37:47 +0600

    data collect from json - implemented

--- cli-task-tracker/src/repositories/TaskRepository.ts
+++ cli-task-tracker/src/repositories/TaskRepository.ts
@@ -1,39 +1,77 @@
 import { Task } from "../models/Task";
+import { promises as fs } from "node:fs";
+import process from "node:process";
+import path from "node:path";
+
 
 export class TaskRepository {
-    private tasks: Task[];
-    constructor(){
-        this.tasks = [];
+    private FILE_PATH = path.join(process.cwd(), "data", "tasks.json");
+    private async readTasks(): Promise<Task[]> {
+        try{
+            const rawData = await fs.readFile(this.FILE_PATH, 'utf-8');
+            const parseData: Task[] = JSON.parse(rawData);
+            for(let task of parseData){
+                task.createdAt = new Date(task.createdAt);
+                task.updatedAt = new Date(task.updatedAt);
+            }
+            return parseData;
+        }catch(error: any) {
+            if(error.code === 'ENOENT'){
+                await fs.mkdir(path.dirname(this.FILE_PATH), {
+                    recursive: true
+                });
+                await fs.writeFile(this.FILE_PATH, JSON.stringify([]));
+                return [];
+            }
+            throw new Error(`Failed to read tasks: ${error.message}`);
+        }
     }
-    save(task: Task): Task{
-        this.tasks.push(task);
+    private async writeTasks(tasks: Task[]): Promise<void> {   
+        try{
+            await fs.writeFile(this.FILE_PATH, JSON.stringify(tasks, null, 2),'utf-8');
+        } catch {
+            throw new Error("tasks writing fails!");
+        }
+    }
+    async save(task: Task): Promise<Task>{
+        const tasks: Task[] = await this.readTasks();
+        tasks.push(task);
+        await this.writeTasks(tasks);
         return task;
     }
-    getAll(): ReadonlyArray<Task> {
-        return this.tasks;
+    async getAll(): Promise<ReadonlyArray<Task>> {
+        const tasks: Task[] = await this.readTasks();
+        return tasks;
     }
-    findById(id: string): Task | undefined{
-        return this.tasks.find(Task => Task.id===id);
-        
+    async findById(id: string): Promise<Task | undefined>{
+        const tasks: Task[] = await this.readTasks();
+        return tasks.find(task => task.id===id);
     }
-    update(id: string, updates: Partial<Task>): Task | null {
-        for(let i=0; i<this.tasks.length; i++) {
-            if(this.tasks[i].id===id){
-                this.tasks[i] = {...this.tasks[i], ...updates};
-                return this.tasks[i];
+    async update(id: string, updates: Partial<Task>): Promise<Task | null> {
+        const tasks: Task[] = await this.readTasks();
+        for(let i=0; i<tasks.length; i++) {
+            if(tasks[i].id===id){
+                tasks[i] = {...tasks[i], ...updates};
+                await this.writeTasks(tasks);
+                return tasks[i];
             }
         }
         return null;
     }
-    delete(id: string): boolean {
+    async delete(id: string): Promise<boolean> {
+        const tasks: Task[] = await this.readTasks();
         let i; 
-        for(i = 1; i<=this.tasks.length; i++){ 
-            if(this.tasks[i].id === id) 
+        for(i = 0; i<tasks.length; i++){ 
+            if(tasks[i].id === id) 
                 break; 
             } 
-        if(i==this.tasks.length) 
+        if(i==tasks.length){
             return false; 
-        else this.tasks.splice(i,1); 
+        } 
+        else{
+            tasks.splice(i,1); 
+            await this.writeTasks(tasks);
             return true;
+        } 
     }
 }
